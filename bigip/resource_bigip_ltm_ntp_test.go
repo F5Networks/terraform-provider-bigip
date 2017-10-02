8a6186887dcab5fd1d678146d7d6d59c63766bae
Sanjay <scshitole@gmail.com>
2017-10-02 14:25:35 -0700

    added test resources

--- bigip/resource_bigip_ltm_ntp_test.go
+++ bigip/resource_bigip_ltm_ntp_test.go
@@ -14,26 +14,25 @@ var TEST_NTP_NAME = fmt.Sprintf("/%s/test-ntp", TEST_PARTITION)
 var TEST_NTP_RESOURCE = `
 resource "bigip_ltm_ntp" "test-ntp" {
 	description = "` + TEST_NTP_NAME + `"
-	description = "/Common/ntp1"
-	servers = ["time.facebook.com"]
+	servers = ["10.10.10.10"]
 	timezone = "America/Los_Angeles"
 }
 `
 
-func TestBigipLtmPool_create(t *testing.T) {
+func TestBigipLtmNtp_create(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAcctPreCheck(t)
 		},
 		Providers:    testAccProviders,
-		CheckDestroy: testCheckPoolsDestroyed,
+		CheckDestroy: testCheckntpsDestroyed,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: TEST_NTP_RESOURCE,
 				Check: resource.ComposeTestCheckFunc(
-					testCheckNtpExists(TEST_NTP_NAME, true),
+					testCheckntpExists(TEST_NTP_NAME, true),
 					resource.TestCheckResourceAttr("bigip_ltm_ntp.test-ntp", "description", TEST_NTP_NAME),
-					resource.TestCheckResourceAttr("bigip_ltm_ntp.test-ntp", "servers", ["time.facebook.com"]),
+					resource.TestCheckResourceAttr("bigip_ltm_ntp.test-ntp", "servers", "10.10.10.10"),
 					resource.TestCheckResourceAttr("bigip_ltm_ntp.test-ntp", "timezone", "America/Los_Angeles"),
 				),
 			},
@@ -47,14 +46,14 @@ func TestBigipLtmNtp_import(t *testing.T) {
 			testAcctPreCheck(t)
 		},
 		Providers:    testAccProviders,
-		CheckDestroy: testCheckNTPDestroyed,
+		CheckDestroy: testCheckntpsDestroyed,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: TEST_NTP_RESOURCE,
 				Check: resource.ComposeTestCheckFunc(
-					testCheckPoolExists(TEST_NTP_NAME, true),
+					testCheckntpExists(TEST_NTP_NAME, true),
 				),
-				ResourceName:      TEST_NTP_RESOURCE,
+				ResourceName:      TEST_NTP_NAME,
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
@@ -62,32 +61,62 @@ func TestBigipLtmNtp_import(t *testing.T) {
 	})
 }
 
-//TODO: test adding/removing nodes
+//var TEST_NODE_IN_POOL_RESOURCE = `
+//resource "bigip_ltm_pool" "test-pool" {
+//	name = "` + TEST_POOL_NAME + `"
+//  	load_balancing_mode = "round-robin"
+//  	nodes = ["${formatlist("%s:80", bigip_ltm_node.*.name)}"]
+//  	allow_snat = false
+//}
+//`
+//func TestBigipLtmNode_removeNode(t *testing.T) {
+//	resource.Test(t, resource.TestCase{
+//		PreCheck: func() {
+//			testAcctPreCheck(t)
+//		},
+//		Providers: testAccProviders,
+//		CheckDestroy: testCheckNodesDestroyed,
+//		Steps: []resource.TestStep{
+//			resource.TestStep{
+//				Config: TEST_NODE_RESOURCE + TEST_NODE_IN_POOL_RESOURCE,
+//				Check: resource.ComposeTestCheckFunc(
+//					testCheckNodeExists(TEST_NODE_NAME, true),
+//					testCheckPoolExists(TEST_POOL_NAME, true),
+//					testCheckPoolMember(TEST_POOL_NAME, TEST_NODE_NAME),
+//				),
+//			},
+//			resource.TestStep{
+//				Config: TEST_NODE_IN_POOL_RESOURCE,
+//				Check: resource.ComposeTestCheckFunc(
+//					testCheckNodeExists(fmt.Sprintf("%s:%s", TEST_NODE_NAME, "80"), false),
+//					testCheckEmptyPool(TEST_POOL_NAME),
+//				),
+//			},
+//		},
+//	})
+//}
 
-func testCheckNtpExists(name string, exists bool) resource.TestCheckFunc {
+func testCheckntpExists(description string, exists bool) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		client := testAccProvider.Meta().(*bigip.BigIP)
 
-		p, err := client.NTPs()
+		ntp, err := client.NTPs()
 		if err != nil {
 			return err
 		}
-		if exists && p == nil {
-			return fmt.Errorf("Ntp ", description, " does not exist.")
+		if exists && ntp == nil {
+			return fmt.Errorf("ntp ", description, " was not created.")
+
 		}
-		if !exists && p != nil {
-			return fmt.Errorf( "Ntp", name, " exists.")
+		if !exists && ntp != nil {
+			return fmt.Errorf("ntp ", description, " still exists.")
+
 		}
 		return nil
 	}
 }
 
-
-
-
-
-
-func testCheckNTPDestroyed(s *terraform.State) error {
+func testCheckntpsDestroyed(s *terraform.State) error {
 	client := testAccProvider.Meta().(*bigip.BigIP)
 
 	for _, rs := range s.RootModule().Resources {
@@ -101,7 +130,8 @@ func testCheckNTPDestroyed(s *terraform.State) error {
 			return err
 		}
 		if ntp != nil {
-			return fmt.Errorf("Ntp ", description, " not destroyed.")
+			return fmt.Errorf("ntp ", description, " not destroyed.")
+
 		}
 	}
 	return nil
---
+++ bigip/resource_bigip_ltm_profile_fasthttp_test.go
@@ -0,0 +1,151 @@
+package bigip
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/scottdware/go-bigip"
+)
+
+var TEST_FASTHTTP_NAME = fmt.Sprintf("/%s/test-fasthttp", TEST_PARTITION)
+
+var TEST_FASTHTTP_RESOURCE = `
+resource "bigip_fasthttp_profile" "test-fasthttp" {
+	name = "` + TEST_FASTHTTP_NAME + `"
+	defaults_from = "/Common/fasthttp"
+            idle_timeout = 300
+            connpoolidle_timeoutoverride	= 0
+            connpool_maxreuse = 2
+            connpool_maxsize  = 2048
+            connpool_minsize = 0
+            connpool_replenish = "enabled"
+            connpool_step = 4
+            forcehttp_10response = "disabled"
+            maxheader_size = 32768
+}
+`
+
+func TestBigipLtmfasthttp_create(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAcctPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckfasthttpsDestroyed,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: TEST_FASTHTTP_RESOURCE,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckfasthttpExists(TEST_FASTHTTP_NAME, true),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "name", TEST_FASTHTTP_NAME),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "defaults_from", "Common/fasthttp"),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "idle_timeout", "300"),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "connpoolidle_timeoutoverride", "0"),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "connpool_maxreuse", "2"),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "connpool_maxsize", "2048"),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "connpool_minsize", "0"),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "connpool_replenish", "enabled"),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "connpool_step", "4"),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "forcehttp_10response", "disabled"),
+					resource.TestCheckResourceAttr("bigip_fasthttp_profile.test-fasthttp", "maxheader_size", "32768"),
+				),
+			},
+		},
+	})
+}
+
+func TestBigipLtmfasthttp_import(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAcctPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckfasthttpsDestroyed,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: TEST_FASTHTTP_RESOURCE,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckfasthttpExists(TEST_FASTHTTP_NAME, true),
+				),
+				ResourceName:      TEST_FASTHTTP_NAME,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+//var TEST_FASTHTTP_IN_POOL_RESOURCE = `
+//resource "bigip_ltm_pool" "test-pool" {
+//	name = "` + TEST_POOL_NAME + `"
+//  	load_balancing_mode = "round-robin"
+//  	fasthttps = ["${formatlist("%s:80", bigip_fasthttp_profile.*.name)}"]
+//  	allow_snat = false
+//}
+//`
+//func TestBigipLtmfasthttp_removefasthttp(t *testing.T) {
+//	resource.Test(t, resource.TestCase{
+//		PreCheck: func() {
+//			testAcctPreCheck(t)
+//		},
+//		Providers: testAccProviders,
+//		CheckDestroy: testCheckfasthttpsDestroyed,
+//		Steps: []resource.TestStep{
+//			resource.TestStep{
+//				Config: TEST_FASTHTTP_RESOURCE + TEST_FASTHTTP_IN_POOL_RESOURCE,
+//				Check: resource.ComposeTestCheckFunc(
+//					testCheckfasthttpExists(TEST_FASTHTTP_NAME, true),
+//					testCheckPoolExists(TEST_POOL_NAME, true),
+//					testCheckPoolMember(TEST_POOL_NAME, TEST_FASTHTTP_NAME),
+//				),
+//			},
+//			resource.TestStep{
+//				Config: TEST_FASTHTTP_IN_POOL_RESOURCE,
+//				Check: resource.ComposeTestCheckFunc(
+//					testCheckfasthttpExists(fmt.Sprintf("%s:%s", TEST_FASTHTTP_NAME, "80"), false),
+//					testCheckEmptyPool(TEST_POOL_NAME),
+//				),
+//			},
+//		},
+//	})
+//}
+
+func testCheckfasthttpExists(name string, exists bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*bigip.BigIP)
+
+		fasthttp, err := client.Fasthttp()
+		if err != nil {
+			return err
+		}
+		if exists && fasthttp == nil {
+			return fmt.Errorf("fasthttp ", name, " was not created.")
+		}
+		if !exists && fasthttp != nil {
+			return fmt.Errorf("fasthttp ", name, " still exists.")
+		}
+		return nil
+	}
+}
+
+func testCheckfasthttpsDestroyed(s *terraform.State) error {
+	client := testAccProvider.Meta().(*bigip.BigIP)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "bigip_fasthttp_profile" {
+			continue
+		}
+
+		name := rs.Primary.ID
+		fasthttp, err := client.Fasthttp()
+		if err != nil {
+			return err
+		}
+		if fasthttp != nil {
+			return fmt.Errorf("fasthttp ", name, " not destroyed.")
+		}
+	}
+	return nil
+}
