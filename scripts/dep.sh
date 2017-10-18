1bfcb2a1f4ce9ccb4f4cb71ed74fd4a3ae0f6cdc
Sanjay <scshitole@gmail.com>
2017-10-18 14:37:12 -0700

    added go get files for dependencies

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -6,11 +6,16 @@ go get github.com/aws/aws-sdk-go/aws
 go get github.com/aws/aws-sdk-go/aws/ec2metadata
 go get github.com/aws/aws-sdk-go/aws/session
 go get github.com/aws/aws-sdk-go/service/s3
- go get github.com/bgentry/speakeasy
+go get github.com/bgentry/speakeasy
 go get github.com/mattn/go-isatty
 go get github.com/posener/complete
 go get github.com/posener/complete/cmd/install
 go get github.com/armon/go-radix
+go get golang.org/x/crypto/openpgp/armor
+go get golang.org/x/crypto/openpgp/errors
+go get golang.org/x/crypto/openpgp/packet
+go get golang.org/x/crypto/openpgp/s2k
+
 
 ls
 cd terraform-provider-f5/
