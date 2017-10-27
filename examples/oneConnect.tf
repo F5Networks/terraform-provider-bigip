b22f9cdd6a204dd0d28188fc4b85c56025f21e0b
Sanjay <scshitole@gmail.com>
2017-10-26 19:29:20 -0700

    fixed tf

--- examples/oneConnect.tf
+++ examples/oneConnect.tf
@@ -1,5 +1,5 @@
 provider "bigip" {
-  address = "10.192.74.61"
+  address = "10.0.1.125"
   username = "admin"
   password = "admin"
 }
@@ -11,11 +11,11 @@ resource "bigip_ltm_oneconnect" "oneconnect-sanjose"
             name = "sanjose"
             partition = "Common"
             defaults_from = "/Common/oneconnect"
-            idler_timeout_override = "disabled"
+            idle_timeout_override = "disabled"
             max_age = 3600
             max_reuse = 1000
             max_size = 1000
-            sharer_pools = "disabled"
+            share_pools = "disabled"
             source_mask = "255.255.255.255"
         }
 
