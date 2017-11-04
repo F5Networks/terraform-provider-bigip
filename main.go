ee59477dea750b6dd96e9ffadb108055991fdf6b
Sanjay <scshitole@gmail.com>
2017-11-04 09:02:44 -0700

    main changes

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-provider-f5/bigip"
+	"github.com/f5devcentral/terraform-provider-f5/bigip"
 )
 
 func main() {
