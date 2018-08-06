3dac10fc5d817e335edfbd02282885f2b8a0b6fa
Sanjay Shitole <scshitole>
2018-08-06 12:23:35 -0700

    change example

--- examples/bigip_ltm_virtual_server.tf
+++ examples/bigip_ltm_virtual_server.tf
@@ -4,19 +4,31 @@ provider "bigip" {
   password = "admin"
 }
 
-resource "bigip_ltm_pool"  "pool" {
-        name = "/Common/terraform-pool"
-        load_balancing_mode = "round-robin"
-        nodes = ["11.1.1.101:80", "11.1.1.102:80"]
-        allow_snat = "yes"
-        allow_nat = "yes"
-}
 
 resource "bigip_ltm_virtual_server" "http" {
-	pool = "/Common/terraform-pool"
-        name = "/Common/terraform_vs_http"
+	pool = "/Common/App1_pool"
+        name = "/Common/App1_vs_http"
 	destination = "100.1.1.100"
 	port = 80
 	source_address_translation = "automap"
 }
 
+resource "bigip_ltm_pool"  "App1_pool" {
+         name = "/Common/App1_pool"
+        load_balancing_mode = "round-robin"
+        monitors = ["/Common/App1_monitor"]
+        allow_snat = true
+       }
+ 
+resource "bigip_ltm_pool_attachment" "attach_node1" {
+        pool = "/Common/App1_pool"
+            node = "/Common/11.1.1.101:80"
+            depends_on = ["bigip_ltm_pool.App1_pool"]
+ 
+}
+resource "bigip_ltm_pool_attachment" "attach_node2" {
+        pool = "/Common/App1_pool"
+            node = "/Common/11.1.1.102:80"
+            depends_on = ["bigip_ltm_pool.App1_pool"]
+ 
+}
