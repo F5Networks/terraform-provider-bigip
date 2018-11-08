ef636a3d861b0b38dcf06b341181600b8429adf6
Sanjay Shitole <scshitole>
2018-11-08 12:01:32 -0800

    updated validators

--- bigip/validators.go
+++ bigip/validators.go
@@ -58,6 +58,34 @@ func validateF5Name(value interface{}, field string) (ws []string, errors []erro
 	return
 }
 
+func validatePoolMemberName(value interface{}, field string) (ws []string, errors []error) {
+	var values []string
+	switch value.(type) {
+	case *schema.Set:
+		values = setToStringSlice(value.(*schema.Set))
+		break
+	case []string:
+		values = value.([]string)
+		break
+	case *[]string:
+		values = *(value.(*[]string))
+		break
+	case string:
+		values = []string{value.(string)}
+		break
+	default:
+		errors = append(errors, fmt.Errorf("Unknown type %v in validatePoolMemberName", reflect.TypeOf(value)))
+	}
+
+	for _, v := range values {
+		match, _ := regexp.MatchString("^\\/[\\w_\\-.]+\\/[\\w_\\-.]+:\\d+$", v)
+		if !match {
+			errors = append(errors, fmt.Errorf("%q must match /Partition/Name and contain letters, numbers or [._-]. e.g. /Common/my-pool", field))
+		}
+	}
+	return
+}
+
 func validateEnabledDisabled(value interface{}, field string) (ws []string, errors []error) {
 	var values []string
 	switch value.(type) {
