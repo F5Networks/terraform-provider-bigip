eef3bf01ab84984fcc8579480980e68ce03366e1
Sanjay Shitole <scshitole>
2019-01-25 13:19:34 -0800

    dev work for #18

--- bigip/resource_bigip_ltm_node.go
+++ bigip/resource_bigip_ltm_node.go
@@ -3,8 +3,6 @@ package bigip
 import (
 	"fmt"
 	"log"
-	"regexp"
-
 	"github.com/f5devcentral/go-bigip"
 	"github.com/hashicorp/terraform/helper/schema"
 )
@@ -75,7 +73,7 @@ func resourceBigipLtmNode() *schema.Resource {
 							Optional:    true,
 							Description: "Specifies the node's address family. The default is 'unspecified', or IP-agnostic",
 						},
-						"name": {
+						"fqdn_name": {
 							Type:        schema.TypeString,
 							Optional:    true,
 							Description: "Specifies the fully qualified domain name of the node.",
@@ -87,9 +85,9 @@ func resourceBigipLtmNode() *schema.Resource {
 							Description: "Specifies the amount of time before sending the next DNS query.",
 						},
 						"downinterval": {
-							Type:        schema.TypeInt,
+							Type:        schema.TypeString,
 							Optional:    true,
-							Default:     5,
+							Default:     "5",
 							Description: "Specifies the number of attempts to resolve a domain name. The default is 5.",
 						},
 						"autopopulate": {
@@ -109,155 +107,36 @@ func resourceBigipLtmNodeCreate(d *schema.ResourceData, meta interface{}) error 
 	client := meta.(*bigip.BigIP)
 
 	name := d.Get("name").(string)
-	address := d.Get("address").(string)
-	rate_limit := d.Get("rate_limit").(string)
-	connection_limit := d.Get("connection_limit").(int)
-	dynamic_ratio := d.Get("dynamic_ratio").(int)
-	monitor := d.Get("monitor").(string)
-	state := d.Get("state").(string)
-
-	r, _ := regexp.Compile("^((?:[0-9]{1,3}.){3}[0-9]{1,3})|(.*:.*)$")
-
-	log.Println("[INFO] Creating node " + name + "::" + address)
-	var err error
-	if r.MatchString(address) {
-		err = client.CreateNode(
-			name,
-			address,
-			rate_limit,
-			connection_limit,
-			dynamic_ratio,
-			monitor,
-			state,
-		)
-	} else {
-		interval := d.Get("fqdn.0.interval").(string)
-		address_family := d.Get("fqdn.0.address_family").(string)
-		autopopulate := d.Get("fqdn.0.autopopulate").(string)
-		downinterval := d.Get("fqdn.0.downinterval").(int)
-
-		err = client.CreateFQDNNode(
-			name,
-			address,
-			rate_limit,
-			connection_limit,
-			dynamic_ratio,
-			monitor,
-			state,
-			interval,
-			address_family,
-			autopopulate,
-			downinterval,
-		)
-	}
-
-	if err != nil {
-		return fmt.Errorf("Error modifying node %s: %v", name, err)
-	}
-
+	p := dataToNode(name, d)
+	d.Partial(true)
+err := client.CreateNode(&p)
+if err != nil {
+	log.Printf("[ERROR] Unable to Create Node  (%s) (%v) ", name, err)
+	return err
+}
+return nil
 	d.SetId(name)
-
+d.SetPartial("fqdn")
+d.Partial(false)
 	return resourceBigipLtmNodeRead(d, meta)
 }
 
 func resourceBigipLtmNodeRead(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*bigip.BigIP)
 
-	name := d.Id()
 
-	log.Println("[INFO] Fetching node " + name)
-
-	node, err := client.GetNode(name)
-	if err != nil {
-		log.Printf("[ERROR] Unable to retrieve node %s  %v :", name, err)
-		return err
-	}
-	if node == nil {
-		log.Printf("[WARN] Node (%s) not found, removing from state", d.Id())
-		d.SetId("")
-		return nil
-	}
-	if node.FQDN.Name != "" {
-		if err := d.Set("address", node.FQDN.Name); err != nil {
-			return fmt.Errorf("[DEBUG] Error saving address to state for Node (%s): %s", d.Id(), err)
-		}
-	} else {
-		// xxx.xxx.xxx.xxx(%x)
-		regex := regexp.MustCompile(`((?:[0-9]{1,3}\.){3}[0-9]{1,3})(?:\%\d+)?`)
-		address := regex.FindStringSubmatch(node.Address)
-		if err := d.Set("address", address[1]); err != nil {
-			return fmt.Errorf("[DEBUG] Error saving address to state for Node (%s): %s", d.Id(), err)
-		}
-	}
-	d.Set("name", name)
-	if err := d.Set("monitor", node.Monitor); err != nil {
-		return fmt.Errorf("[DEBUG] Error saving Monitor to state for Node (%s): %s", d.Id(), err)
-	}
-	if err := d.Set("rate_limit", node.RateLimit); err != nil {
-		return fmt.Errorf("[DEBUG] Error saving Monitor to state for Node (%s): %s", d.Id(), err)
-	}
-
-	d.Set("connection_limit", node.ConnectionLimit)
-	d.Set("dynamic_ratio", node.DynamicRatio)
-	d.Set("fqdn.0.interval", node.FQDN.Interval)
-	d.Set("fqdn.0.downinterval", node.FQDN.DownInterval)
-	d.Set("fqdn.0.autopopulate", node.FQDN.AutoPopulate)
-	d.Set("fqdn.0.address_family", node.FQDN.AddressFamily)
 
 	return nil
 }
 
 func resourceBigipLtmNodeExists(d *schema.ResourceData, meta interface{}) (bool, error) {
-	client := meta.(*bigip.BigIP)
 
-	name := d.Id()
-	log.Println("[INFO] Fetching node " + name)
 
-	node, err := client.GetNode(name)
-	if err != nil {
-		log.Printf("[ERROR] Unable to retrieve node %s  %v :", name, err)
-		return false, err
-	}
-
-	if node == nil {
-		log.Printf("[WARN] node (%s) not found, removing from state", d.Id())
-		return false, nil
-	}
-	return node != nil, nil
+	return true, nil
 }
 
 func resourceBigipLtmNodeUpdate(d *schema.ResourceData, meta interface{}) error {
-	client := meta.(*bigip.BigIP)
 
-	name := d.Id()
-	address := d.Get("address").(string)
-	r, _ := regexp.Compile("^((?:[0-9]{1,3}.){3}[0-9]{1,3})|(.*:.*)$")
-
-	var node *bigip.Node
-	if r.MatchString(address) {
-		node = &bigip.Node{
-			Address:         address,
-			ConnectionLimit: d.Get("connection_limit").(int),
-			DynamicRatio:    d.Get("dynamic_ratio").(int),
-			Monitor:         d.Get("monitor").(string),
-			RateLimit:       d.Get("rate_limit").(string),
-			State:           d.Get("state").(string),
-		}
-	} else {
-		node = &bigip.Node{
-			ConnectionLimit: d.Get("connection_limit").(int),
-			DynamicRatio:    d.Get("dynamic_ratio").(int),
-			Monitor:         d.Get("monitor").(string),
-			RateLimit:       d.Get("rate_limit").(string),
-			State:           d.Get("state").(string),
-		}
-
-		err := client.ModifyNode(name, node)
-		if err != nil {
-			return fmt.Errorf("Error modifying node %s: %v", name, err)
-		}
-	}
-	return resourceBigipLtmNodeRead(d, meta)
+	return nil
 }
 
 func resourceBigipLtmNodeDelete(d *schema.ResourceData, meta interface{}) error {
@@ -273,4 +152,29 @@ func resourceBigipLtmNodeDelete(d *schema.ResourceData, meta interface{}) error 
 	}
 	d.SetId("")
 	return nil
+}
+
+func dataToNode(name string, d *schema.ResourceData) bigip.Node {
+	var p bigip.Node
+	p.Name = name
+	p.Address = d.Get("address").(string)
+	p.RateLimit = d.Get("rate_limit").(string)
+	p.ConnectionLimit = d.Get("connection_limit").(int)
+	p.DynamicRatio = d.Get("dynamic_ratio").(int)
+	p.Monitor = d.Get("monitor").(string)
+	p.State = d.Get("state").(string)
+	fqdnCount := d.Get("fqdn.#").(int)
+	p.Fqdn = make([]bigip.Fqdnrecord, 0, fqdnCount)
+	//p.Fqdn = setToStringSlice(d.Get("fqdn").(*schema.Set))
+
+	for i := 0; i < fqdnCount; i++ {
+		var r bigip.Fqdnrecord
+		prefix := fmt.Sprintf("fqdn.%d", i)
+		r.FqdnName = d.Get(prefix + ".fqdn_name").(string)
+		r.Interval = d.Get(prefix + ".interval").(string)
+		p.Fqdn = append(p.Fqdn, r)
+	}
+	log.Println("I am in data to Node value of p                                                   ", p)
+
+	return p
 }
