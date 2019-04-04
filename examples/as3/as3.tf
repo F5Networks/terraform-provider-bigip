956599aebdc561cdbd107a1280b9659102500c2d
Sanjay Shitole <scshitole>
2019-04-03 17:56:46 -0700

    as3 example resource

--- examples/as3/as3.tf
+++ examples/as3/as3.tf
@@ -1,14 +1,17 @@
+//We can use null_resource to  deploy As3 templates, below is simple example to install the as3 rpm and another resource which deploys the example1.json ( which has the http VS configuration) More details on As3 please refer to https://clouddocs.f5.com/products/extensions/f5-appsvcs-extension/latest/
+
+
 provider "bigip" {
-  address = "54.177.207.51"
+  address = "X.X.X.X"
   username = "admin"
-  password = "1LStm3545"
+  password = "pass"
 }
 
 
 // As3 using terraform provisoner
 resource "null_resource" "install_as3" {
   provisioner "local-exec" {
-    command = "sh install_as3.sh 54.177.207.51 admin:1LStm3545 f5-appsvcs-3.9.0-3.noarch.rpm"
+    command = "sh install_as3.sh X.X.X.X  admin:pass f5-appsvcs-3.9.0-3.noarch.rpm"
   }
 }
  resource "null_resource" "deploy_as3_http" {
