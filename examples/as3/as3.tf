434ca23828bac21d1141be3fff57d70bafa0af45
Sanjay Shitole <scshitole>
2019-04-04 10:13:52 -0700

    as3 tf update

--- examples/as3/as3.tf
+++ examples/as3/as3.tf
@@ -14,6 +14,7 @@ resource "null_resource" "install_as3" {
     command = "sh install_as3.sh X.X.X.X  admin:pass f5-appsvcs-3.9.0-3.noarch.rpm"
   }
 }
+// The below null resource can be used to deploy HTTP script as3_http.sh which uses JSON payload example1.josn 
  resource "null_resource" "deploy_as3_http" {
  provisioner "local-exec" {
     command = "sh as3_http.sh"
