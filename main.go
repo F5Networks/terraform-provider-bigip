948d0b3b723ec8b82e763f5adf8778252341109e
Hoffmann <Travis.Hoffmann@advicent.com>
2019-07-15 17:27:31 -0500

    switched main.go repos to my repos for testing

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-providers/terraform-provider-bigip/bigip"
+	"github.com/pirotrav/terraform-provider-bigip/bigip"
 )
 
 func main() {
