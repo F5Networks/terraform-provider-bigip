d83c47c0b4684f2faf435135679fdd5584afdfc6
Joe Jordan <j.jordan@f5.com>
2019-09-27 15:55:56 -0700

    removed passive voice check

--- scripts/test-docs.sh
+++ scripts/test-docs.sh
@@ -4,10 +4,11 @@ set -e
 set -x
 
 echo "Building docs with Sphinx"
+make clean
 make html
 
 echo "Checking grammar and style"
-write-good `find ./docs -not \( -path ./docs/drafts -prune \) -name '*.rst'` --passive --so --no-illusion --thereIs --cliches 
+write-good `find ./docs -not \( -path ./docs/drafts -prune \) -name '*.rst'` --so --no-illusion --thereIs --cliches 
 
 echo "Checking links"
 make linkcheck
