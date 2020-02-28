f24e2564be2370413f1ff2d9cbf918daf5942717
Saketha GADDAM <sgaddam@hyd-ml-00064444.olympus.f5net.com>
2020-02-28 12:28:26 +0530

    adding gtm wideip delete method

--- bigip/provider.go
+++ bigip/provider.go
@@ -100,7 +100,7 @@ func Provider() terraform.ResourceProvider {
 			"bigip_do":                              resourceBigipDo(),
 			"bigip_ssl_certificate":                 resourceBigipSslCertificate(),
 			"bigip_ssl_key":                         resourceBigipSslKey(),
-                        "bigip_gtm_wideip":                      resourceBigipGtmWideip(),
+			"bigip_gtm_wideip":                      resourceBigipGtmWideip(),
 		},
 
 		ConfigureFunc: providerConfigure,
