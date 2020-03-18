626d71eb9cca7cf0c4f20eeb2af7c48f5edf5537
RavinderReddyF5 <r.chinthalapalli@f5.com>
2020-03-18 14:29:15 +0530

    updated examples for terraform v0.12 compatible

--- examples/bigip_ltm_irule.tf
+++ examples/bigip_ltm_irule.tf
@@ -4,44 +4,43 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 provider "bigip" {
-  address = "xxx.xxx.xxx.xxx"
+  address  = "xxx.xxx.xxx.xxx"
   username = "xxxx"
   password = "xxxx"
 }
 
 #Loading from a file is the preferred method
 resource "bigip_ltm_irule" "rule" {
-  name = "/Common/terraform_irule"
-  irule = "${file("myirule.tcl")}"
+  name  = "/Common/terraform_irule"
+  irule = file("myirule.tcl")
 }
 
 resource "bigip_ltm_monitor" "monitor" {
-        name = "/Common/terraform_monitor"
-        parent = "/Common/http"
-        send = "GET /some/path\r\n"
-        timeout = "999"
-        interval = "999"
+  name     = "/Common/terraform_monitor"
+  parent   = "/Common/http"
+  send     = "GET /some/path\r\n"
+  timeout  = "999"
+  interval = "999"
 }
 
-resource "bigip_ltm_pool"  "pool" {
-        name = "/Common/terraform-pool"
-        load_balancing_mode = "round-robin"
-        nodes = ["11.1.1.101:80", "11.1.1.102:80"]
-        monitors = ["/Common/terraform_monitor"]
-        allow_snat = true
+resource "bigip_ltm_pool" "pool" {
+  name                = "/Common/terraform-pool"
+  load_balancing_mode = "round-robin"
+
+  #nodes = ["11.1.1.101:80", "11.1.1.102:80"]
+  monitors   = ["/Common/terraform_monitor"]
+  allow_snat = true
 }
 
 resource "bigip_ltm_virtual_server" "http" {
-	pool = "/Common/terraform-pool"
-        name = "/Common/terraform_vs_http"
-	destination = "100.1.1.100"
-	port = 80
-        ip_protocol = "tcp"
-        profiles = ["/Common/sanjay-http"]
-        irules = ["/Common/terraform_irule"]
-	source_address_translation = "automap"
-        depends_on = ["bigip_ltm_pool.pool"]
+  pool                       = "/Common/terraform-pool"
+  name                       = "/Common/terraform_vs_http"
+  destination                = "100.1.1.100"
+  port                       = 80
+  ip_protocol                = "tcp"
+  profiles                   = ["/Common/sanjay-http"]
+  irules                     = ["/Common/terraform_irule"]
+  source_address_translation = "automap"
+  depends_on                 = [bigip_ltm_pool.pool]
 }
-
-
 
