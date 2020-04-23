ec76df8877e61fcbf3977ca65678b3266a9722a3
GaddamSaketha <gaddam.saketha@gmail.com>
2020-04-23 10:27:35 +0530

    adding changes to tenant filter

--- bigip/resource_bigip_as3.go
+++ bigip/resource_bigip_as3.go
@@ -37,17 +37,17 @@ func resourceBigipAs3() *schema.Resource {
 				},
 				ValidateFunc: validation.ValidateJsonString,
 			},
-                        "tenant_name": {
-                               Type:        schema.TypeString,
-                                Optional:    true, 
-                                Deprecated:    "this attribute is no longer in use",
-                                Description: "Name of Tenant",
-                        },
-                        "tenant_filter": {
-                                Type:        schema.TypeString,
-                                Optional:    true,
-                                Description: "Name of Tenant",
-                        },
+			"tenant_name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Deprecated:  "this attribute is no longer in use",
+				Description: "Name of Tenant",
+			},
+			"tenant_filter": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Name of Tenant",
+			},
 			"tenant_list": {
 				Type:        schema.TypeString,
 				Computed:    true,
@@ -61,15 +61,15 @@ func resourceBigipAs3() *schema.Resource {
 func resourceBigipAs3Create(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*bigip.BigIP)
 	as3Json := d.Get("as3_json").(string)
-        tenantFilter := d.Get("tenant_filter").(string)
+	tenantFilter := d.Get("tenant_filter").(string)
 	if ok := bigip.ValidateAS3Template(as3Json); !ok {
 		return fmt.Errorf("[AS3] Error validating template \n")
 	}
 	//strTrimSpace := strings.TrimSpace(as3Json)
 	tenantList, _ := client.GetTenantList(as3Json)
-        if tenantFilter != "" {
-           tenantList = tenantFilter
-        }
+	if tenantFilter != "" {
+		tenantList = tenantFilter
+	}
 	d.Set("tenant_list", tenantList)
 	strTrimSpace := client.AddTeemAgent(as3Json)
 	log.Printf("[INFO] Tenants in Json:%+v", tenantList)
@@ -105,10 +105,10 @@ func resourceBigipAs3Exists(d *schema.ResourceData, meta interface{}) (bool, err
 	client := meta.(*bigip.BigIP)
 	log.Printf("[INFO] Checking if As3 config exists in BIGIP")
 	name := d.Get("tenant_list").(string)
-        tenantFilter := d.Get("tenant_filter").(string)
-        if tenantFilter!= "" {
-           name = tenantFilter
-        }
+	tenantFilter := d.Get("tenant_filter").(string)
+	if tenantFilter != "" {
+		name = tenantFilter
+	}
 	as3Resp, err := client.GetAs3(name)
 	if err != nil {
 		log.Printf("[ERROR] Unable to retrieve json ")
@@ -128,28 +128,31 @@ func resourceBigipAs3Update(d *schema.ResourceData, meta interface{}) error {
 	log.Printf("[INFO] Updating As3 Config :%s", as3Json)
 	name := d.Get("tenant_list").(string)
 	tenantList, _ := client.GetTenantList(as3Json)
-        tenantFilter := d.Get("tenant_filter").(string)
-        if tenantFilter == "" {
-	if tenantList != name {
-		d.Set("tenant_list", tenantList)
-		new_list := strings.Split(tenantList, ",")
-		old_list := strings.Split(name, ",")
-		deleted_tenants := client.TenantDifference(old_list, new_list)
-		if deleted_tenants != "" {
-			err := client.DeleteAs3Bigip(deleted_tenants)
-			if err != nil {
-				log.Printf("[ERROR] Unable to Delete removed tenants: %v :", err)
-				return err
+	tenantFilter := d.Get("tenant_filter").(string)
+	if tenantFilter == "" {
+		if tenantList != name {
+			d.Set("tenant_list", tenantList)
+			new_list := strings.Split(tenantList, ",")
+			old_list := strings.Split(name, ",")
+			deleted_tenants := client.TenantDifference(old_list, new_list)
+			if deleted_tenants != "" {
+				err := client.DeleteAs3Bigip(deleted_tenants)
+				if err != nil {
+					log.Printf("[ERROR] Unable to Delete removed tenants: %v :", err)
+					return err
+				}
 			}
 		}
-	}
-        } else {
-           tenantList = tenantFilter
-        }
-	err := client.ModifyAs3(tenantList, as3Json)
-
-	if err != nil {
-		return fmt.Errorf("Error modifying json %s: %v", name, err)
+		err := client.ModifyAs3(tenantList, as3Json)
+		if err != nil {
+			return fmt.Errorf("Error modifying json %s: %v", name, err)
+		}
+	} else {
+		tenantList = tenantFilter
+		err := client.PostAs3Bigip(as3Json, tenantList)
+		if err != nil {
+			return fmt.Errorf("Error modifying json %s: %v", name, err)
+		}
 	}
 	return resourceBigipAs3Read(d, meta)
 }
