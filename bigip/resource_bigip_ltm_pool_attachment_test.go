1fa53311b9342699cd2859e33ea8c16dc96185f8
GaddamSaketha <gaddam.saketha@gmail.com>
2020-06-10 11:24:36 +0530

    adding basic acceptance tests for ltm pool attachment

--- bigip/resource_bigip_ltm_pool_attachment_test.go
+++ bigip/resource_bigip_ltm_pool_attachment_test.go
@@ -95,6 +95,7 @@ resource "bigip_ltm_pool" "test-pool" {
         reselect_tries = "2"
 }
 `
+
 func TestAccBigipLtmPoolAttachment_create(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
@@ -107,120 +108,119 @@ func TestAccBigipLtmPoolAttachment_create(t *testing.T) {
 				Config: TEST_POOL_RESOURCE1,
 				Check: resource.ComposeTestCheckFunc(
 					testCheckPoolExists(TEST_POOL_NAME, true),
-                                        testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, true),
+					testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, true),
 					resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "pool", TEST_POOL_NAME),
 					resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "node", TEST_POOLNODE_NAMEPORT),
 				),
 			},
 		},
 	})
-        resource.Test(t, resource.TestCase{
-                PreCheck: func() {
-                        testAcctPreCheck(t)
-                },
-                Providers:    testAccProviders,
-                CheckDestroy: testCheckPoolsDestroyed,
-                Steps: []resource.TestStep{
-                        {
-                                Config: TEST_POOL_RESOURCE,
-                                Check: resource.ComposeTestCheckFunc(
-                                        testCheckPoolExists(TEST_POOL_NAME, true),
-                                        testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, true),
-                                        resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "pool", TEST_POOL_NAME),
-                                        resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "node", TEST_POOLNODE_NAMEPORT),
-                                ),
-                        },
-                },
-        })
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAcctPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckPoolsDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: TEST_POOL_RESOURCE,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckPoolExists(TEST_POOL_NAME, true),
+					testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, true),
+					resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "pool", TEST_POOL_NAME),
+					resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "node", TEST_POOLNODE_NAMEPORT),
+				),
+			},
+		},
+	})
 }
 
 func TestAccBigipLtmPoolAttachment_update(t *testing.T) {
-        resource.Test(t, resource.TestCase{
-                PreCheck: func() {
-                        testAcctPreCheck(t)
-                },
-                Providers:    testAccProviders,
-                CheckDestroy: testCheckPoolsDestroyed,
-                Steps: []resource.TestStep{
-                        {
-                                Config: TEST_POOL_RESOURCE1,
-                                Check: resource.ComposeTestCheckFunc(
-                                        testCheckPoolExists(TEST_POOL_NAME, true),
-                                        testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, true),
-                                        resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "pool", TEST_POOL_NAME),
-                                        resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "node", TEST_POOLNODE_NAMEPORT),
-                                ),
-                        },
-                        {
-                                Config: TEST_POOL_RESOURCE2,
-                                Check: resource.ComposeTestCheckFunc(
-                                        testCheckPoolExists(TEST_POOL_NAME, true),
-                                        testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, false),
-                                ),
-                        },
-                },
-        })
-        resource.Test(t, resource.TestCase{
-                PreCheck: func() {
-                        testAcctPreCheck(t)
-                },
-                Providers:    testAccProviders,
-                CheckDestroy: testCheckPoolsDestroyed,
-                Steps: []resource.TestStep{
-                        {
-                                Config: TEST_POOL_RESOURCE,
-                                Check: resource.ComposeTestCheckFunc(
-                                        testCheckPoolExists(TEST_POOL_NAME, true),
-                                        testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, true),
-                                        resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "pool", TEST_POOL_NAME),
-                                        resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "node", TEST_POOLNODE_NAMEPORT),
-                                ),
-                        },
-                        {
-                                Config: TEST_POOL_RESOURCE3,
-                                Check: resource.ComposeTestCheckFunc(
-                                        testCheckPoolExists(TEST_POOL_NAME, true),
-                                        testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, false),
-                                ),
-                        },
-                },
-        })
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAcctPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckPoolsDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: TEST_POOL_RESOURCE1,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckPoolExists(TEST_POOL_NAME, true),
+					testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, true),
+					resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "pool", TEST_POOL_NAME),
+					resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "node", TEST_POOLNODE_NAMEPORT),
+				),
+			},
+			{
+				Config: TEST_POOL_RESOURCE2,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckPoolExists(TEST_POOL_NAME, true),
+					testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, false),
+				),
+			},
+		},
+	})
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAcctPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckPoolsDestroyed,
+		Steps: []resource.TestStep{
+			{
+				Config: TEST_POOL_RESOURCE,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckPoolExists(TEST_POOL_NAME, true),
+					testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, true),
+					resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "pool", TEST_POOL_NAME),
+					resource.TestCheckResourceAttr("bigip_ltm_pool_attachment.test-pool_test-node", "node", TEST_POOLNODE_NAMEPORT),
+				),
+			},
+			{
+				Config: TEST_POOL_RESOURCE3,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckPoolExists(TEST_POOL_NAME, true),
+					testCheckPoolAttachment(TEST_POOL_NAME, TEST_POOLNODE_NAMEPORT, false),
+				),
+			},
+		},
+	})
 }
-func testCheckPoolAttachment(poolName string, expected string, exists bool) resource.TestCheckFunc  {
-        return func(s *terraform.State) error {
-        client := testAccProvider.Meta().(*bigip.BigIP)    
+func testCheckPoolAttachment(poolName string, expected string, exists bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*bigip.BigIP)
 
-	pool, err := client.GetPool(poolName)
-	if err != nil {
-		return err
-	}
-	if pool == nil {
-               return fmt.Errorf("Pool %s does not exist.", poolName)
-	}
-
-	nodes, err := client.PoolMembers(poolName)
-	if err != nil {
-		return fmt.Errorf("Error retrieving pool (%s) members: %s", poolName, err)
-	}
-	if nodes == nil {
-		return fmt.Errorf("Pool member %s does not exist.", expected)
-	}
-	found := false
-	for _, node := range nodes.PoolMembers {
-		if expected == node.FullPath {
-			found = true
-			break
+		pool, err := client.GetPool(poolName)
+		if err != nil {
+			return err
 		}
+		if pool == nil {
+			return fmt.Errorf("Pool %s does not exist.", poolName)
+		}
+
+		nodes, err := client.PoolMembers(poolName)
+		if err != nil {
+			return fmt.Errorf("Error retrieving pool (%s) members: %s", poolName, err)
+		}
+		if nodes == nil {
+			return fmt.Errorf("Pool member %s does not exist.", expected)
+		}
+		found := false
+		for _, node := range nodes.PoolMembers {
+			if expected == node.FullPath {
+				found = true
+				break
+			}
+		}
+
+		if !found && exists {
+			return fmt.Errorf("Node %s is not a member of pool %s", expected, poolName)
+		}
+		if found && !exists {
+			return fmt.Errorf("Node %s is still  a member of pool %s", expected, poolName)
+		}
+
+		return nil
 	}
-
-	if !found && exists{
-		return fmt.Errorf("Node %s is not a member of pool %s", expected, poolName)
-	}
-        if found && !exists{
-                return fmt.Errorf("Node %s is still  a member of pool %s", expected, poolName)
-        }
-
-	return nil
 }
-}
-
