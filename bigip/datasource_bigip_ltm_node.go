e2b651e6c6eb611923f3011a027152a47716b080
dinesh <d.papineni@f5.com>
2021-02-18 18:55:41 +0530

    Restore Max items in node data source

--- bigip/datasource_bigip_ltm_node.go
+++ bigip/datasource_bigip_ltm_node.go
@@ -71,7 +71,7 @@ func dataSourceBigipLtmNode() *schema.Resource {
 				Type:     schema.TypeList,
 				Optional: true,
 				Computed: true,
-				//MaxItems: 1,
+				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"address_family": {
