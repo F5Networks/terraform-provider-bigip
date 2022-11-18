8a8dc957276d1f47232c9128c060e494a0a60a29
Vanessa Trippel <v.trippel@f5.com>
2022-11-18 11:55:43 -0800

    adding BIG-IP to breadcrumbs

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ source_encoding = 'utf-8-sig'
 master_doc = 'index'
 
 # General information about the project.
-project = u'F5 Resources for Terraform'
+project = u'F5 BIG-IP Resources for Terraform'
 copyright = u'2022 F5 Networks Inc'
 author = u'F5 Networks'
 
